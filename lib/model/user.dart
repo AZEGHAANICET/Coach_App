c7da0ae1aaa9238ffa80f9f540fd8041503cf95f
azegha <loicanicetazegha@gmail.com>
2023-10-19 19:52:15 +0200

    modification connexion page admin and customer

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,11 +1,35 @@
-class User {
-  
-  late String email ;
-  late String motDepasse;
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserModel {
+  late String email;
+  late String motDePasse;
   late bool isAdmin;
   late String uid;
 
+  UserModel({
+    required String email,
+    required bool isAdmin,
+    required String uid,
+  }) {
+    this.email = email;
+    this.motDePasse = motDePasse;
+    this.isAdmin = isAdmin;
+    this.uid = uid;
+  }
 
-  
-  User({required this.email, required this.motDepasse, required this.uid});
+  static UserModel? fromFirebaseUser(User? user) {
+    if (user == null) {
+      return null;
+    }
+    return UserModel(
+      email: user.email ?? '',
+      isAdmin:
+          false, // Ajoutez votre logique pour déterminer si l'utilisateur est administrateur
+      uid: user.uid,
+    );
+  }
+
+  void setisAdmin(bool admin) {
+    this.isAdmin = admin;
+  }
 }
