d8e90396baed473c8ca89ccbfc3afee2b3304aee
AZEGHAANICET <116329484+AZEGHAANICET@users.noreply.github.com>
2023-10-19 11:16:57 +0200

    Update authentication.dart

--- lib/screens/authentication.dart
+++ lib/screens/authentication.dart
@@ -25,20 +25,17 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
     if (!isValidForm || !_isLogin) {
       return;
     }
-
     _form.currentState!.save();
       setState(() {
         _isAuthenticated = true;
       });
       if (_isLogin) {
-        signIn(_email_enter,_password);
+      _authService.signIn(_email_enter, _password);
       } else {
-        signUpWithEmail(_email_enter,_password);
+     _authService.signUpWithEmail(_email_enter, _password);
       }
-   
-
     setState(() {
-      _isAuthenticated = true;
+      _isAuthenticated = false;
     });
   }
 
@@ -148,96 +145,4 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
   }
 
 
-
-Future<void> signUpWithEmail(String email, String password) async {
-  try {
-    UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    await sendEmailVerification(email);
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Inscription réussie. Un e-mail de vérification a été envoyé.')),
-    );
-  } on FirebaseAuthException catch (error) {
-    String errorMessage = 'Une erreur d\'inscription s\'est produite.';
-
-    if (error.code == 'email-already-in-use') {
-      errorMessage = 'Cette adresse e-mail est déjà utilisée.';
-    }
-    showSnackBar(content: Text(errorMessage));
-  }
-}
-
-Future<void> sendEmailVerification(String email) async {
-  User? user = FirebaseAuth.instance.currentUser;
-  await user?.sendEmailVerification();
-}
-  
-Future<void> signIn(String email, String password) async {
-  try {
-    UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    if(userCredential.user!=null && userCredential.user!.emailVerified){
-      showSnackBar(content: Text('Connexion effectuée avec succès'));
-    }
-    else{
-       showSnackBar(content: Text('Echec de connexion vous n\'avez pas vérifié votre email lors de votre inscription'));
-    }
-  } on FirebaseAuthException catch (error) {
-    String errorMessage = 'Une erreur d\'authentification s\'est produite.';
-
-    if (error.code == 'user-not-found') {
-      errorMessage = 'Aucun utilisateur trouvé avec cette adresse e-mail.';
-    } else if (error.code == 'wrong-password') {
-      errorMessage = 'Le mot de passe est incorrect.Try again!!';
-    }
-    showSnackBar(content: Text(errorMessage));
-  }
-}
-
-
-  
-  bool isValidEmail(String email) {
-    // Expression régulière pour valider une adresse e-mail
-    final RegExp emailRegex = RegExp(
-      r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
-    );
-    return emailRegex.hasMatch(email);
-  }
-
-  bool isPasswordValid(String password) {
-    if (password == null || password.isEmpty) {
-      return false;
-    }
-    if (password.length < 8) {
-      return false;
-    }
-    if (!password.contains(RegExp(r'[A-Z]'))) {
-      return false;
-    }
-    if (!password.contains(RegExp(r'[a-z]'))) {
-      return false;
-    }
-    if (!password.contains(RegExp(r'[0-9]'))) {
-      return false;
-    }
-    return true;
-  }
-
-
-
-
-
-  void showSnackBar({required Widget content}) {
-  ScaffoldMessenger.of(context).clearSnackBars();
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      content: content,
-      duration: Duration(seconds: 3), 
-    ),
-  );
-}
 }
