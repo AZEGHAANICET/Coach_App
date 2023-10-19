82dad1f2a3842c69f3ba40d7754dbeb5593c51a4
AZEGHAANICET <116329484+AZEGHAANICET@users.noreply.github.com>
2023-10-19 11:26:42 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_coach_app/screens/admin.dart';
+import 'package:flutter_coach_app/screens/customer.dart';
 import 'package:flutter_coach_app/screens/authentication.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -19,15 +20,19 @@ class App extends StatelessWidget {
       title: "Flutter App",
       theme: ThemeData(primarySwatch: Colors.blue),
       darkTheme: ThemeData.dark(),
-      home: StreamBuilder(
+      home: StreamBuilder<User?>(
           stream: FirebaseAuth.instance.authStateChanges(),
           builder: (ctx, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return CircularProgressIndicator();
             }
-            if (snapshot.hasData) {
-              return Admin();
+           if (snapshot.hasError) {
+              return Text('Une erreur s\'est produite!!');
             }
+            if(snapshot.hasData){
+               bool isAdmin = snapshot.data?.isAdmin??false;
+               return isAdmin?Admin():Customer();
+             }
             return AuthenticationScreen();
           }),
     );
